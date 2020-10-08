3a027cd6c7665a496a579e689aebb516d0013499
Crowdbotics <team@crowdbotics.com>
2020-10-08 13:54:30 +0000

    Update components

--- src/features/BlankScreen011919/screens/blank.js
+++ src/features/BlankScreen011919/screens/blank.js
@@ -31,36 +31,52 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.View_1}>
-      <Text style={styles.Text_2}>Sample text content</Text>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen211921")}
+      >
+        <Text style={styles.Text_2}>Sample text content</Text>
+      </TouchableOpacity>
       <Button
         title="Press me!"
         color="#3366FF"
         style={styles.Button_3}
-        onPress={() => alert("Pressed!")}
+        onPress={() => this.props.navigation.navigate("BlankScreen111920")}
       />
-      <DateTimePicker
-        showIcon={false}
-        style={styles.DateTimePicker_4}
-        date={this.state.DateTimePicker_4}
-        onDateChange={selectedDate =>
-          this.setState({ DateTimePicker_4: selectedDate })
-        }
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        editable={true}
-        style={styles.TextInput_5}
-        value={this.state.TextInput_5}
-        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
-      />
-      <TextInput
-        placeholder="Number Input Placeholder"
-        editable={true}
-        keyboardType="numeric"
-        style={styles.TextInput_6}
-        value={this.state.TextInput_6}
-        onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
-      />
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen311922")}
+      >
+        <DateTimePicker
+          showIcon={false}
+          style={styles.DateTimePicker_4}
+          date={this.state.DateTimePicker_4}
+          onDateChange={selectedDate =>
+            this.setState({ DateTimePicker_4: selectedDate })
+          }
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen111920")}
+      >
+        <TextInput
+          placeholder="Sample text input placeholder"
+          editable={true}
+          style={styles.TextInput_5}
+          value={this.state.TextInput_5}
+          onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen211921")}
+      >
+        <TextInput
+          placeholder="Number Input Placeholder"
+          editable={true}
+          keyboardType="numeric"
+          style={styles.TextInput_6}
+          value={this.state.TextInput_6}
+          onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -69,6 +85,195 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginHorizontal: 16
+  },
+
+  View_1: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+
+  Text_2: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+
+  Button_3: {
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+
+  DateTimePicker_4: {
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+
+  TextInput_5: {
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+
+  TextInput_6: {
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   },
   View_1: {
     marginLeft: 0,
